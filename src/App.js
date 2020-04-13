d2bd68eb87eecf7adf06e8fa44374ed247e95794
Fernando Torres <webmasterft@users.noreply.github.com>
2020-04-12 21:19:40 -0500

    Finish
    All videos and code ready

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, { useState } from "react";
-import "./styles.css";
-import VideoListView from "./videoListView";
-import { videoList } from "./videosList";
-import Video from "./video";
+import React, { useState } from 'react';
+import './styles.css';
+import VideoListView from './videoListView';
+import { videoList } from './videosList';
+import Video from './video';
 
 export default function App() {
   const [currentVideo, setCurrent] = useState(0);
   return (
     <div className="App container mb-5 mt-5">
       <div className="row">
+        <h1 className="mb-5"> Manual de administración sitio web ASOPREOL</h1>
         <div className="col-md-6">
-          <VideoListView currentVideo={currentVideo}
-          videoList={videoList} setCurrent={setCurrent} />
+          <VideoListView
+            currentVideo={currentVideo}
+            videoList={videoList}
+            setCurrent={setCurrent}
+          />
         </div>
         <div className="col-md-6">
           <Video video={videoList[currentVideo]} />
--- src/videosList.js
+++ src/videosList.js
@@ -4,7 +4,27 @@ export const videoList = [
     title: "Primeros pasos"
   },
   {
+    id: "uRZHdZJJZGg",
+    title: "Editar Home"
+  },
+  {
+    id: "giak3lPecB0",
+    title: "Editar Footer"
+  },
+  {
     id: "EFXyZPkrj1Y",
     title: "Editar contenido"
+  },
+  {
+    id: "EDmABBBt_Bw",
+    title: "Editar tablas"
+  },
+  {
+    id: "hpgUNnlT8y0",
+    title: "Editar y subir PDF"
+  },
+  {
+    id: "wJjQxI6RYto",
+    title: "Menu - crear,editar,borrar"
   }
 ];
